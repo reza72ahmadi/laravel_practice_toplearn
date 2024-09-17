2f3e7baa8b05c0b1dc5a175532b4b0d5aed5e8fe
reza72ahmadi <reza72.ahmadi@gmail.com>
2024-09-17 23:12:30 +0430

    category made and added

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,25 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\Market\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::prefix('admin')->namespace('Admin')->group(function () {
+
     Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.home');
+    Route::prefix('market')->namespace('Market')->group(function () {
+        Route::prefix('category')->group(function () {
+            
+            Route::get('/', [CategoryController::class, 'index'])->name('admin.market.category');
+            Route::get('/create', [CategoryController::class, 'index'])->name('admin.market.category.create');
+            Route::post('/store', [CategoryController::class, 'store'])->name('admin.market.category.store');
+            Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('admin.market.category.edit');
+            Route::put('/update/{id}', [CategoryController::class, 'update'])->name('admin.market.category.update');
+            Route::delete('/delete/{id}', [CategoryController::class, 'destroy'])->name('admin.market.category.destroy');
+        });
+    });
 });
